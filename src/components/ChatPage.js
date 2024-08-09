a935c2cb7a1b20da1b4755e99fde2dbec47a43a5
Prathit <67639393+Prathit-tech@users.noreply.github.com>
2024-08-09 19:56:04 +0530

    analytics added to chatpage

--- src/components/ChatPage.js
+++ src/components/ChatPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { SparklesIcon, SearchIcon, Loader, ToggleLeft, ToggleRight } from 'lucide-react';
 import axios from 'axios';
+import { Analytics } from "@vercel/analytics/react";
 
 const ChatPage = () => {
   const [docLink, setDocLink] = useState('');
