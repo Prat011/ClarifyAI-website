6e5d1e3da54523525604e0e39f4b5555ab583841
Prathit <67639393+Prathit-tech@users.noreply.github.com>
2024-09-28 11:05:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import ChatPage from './components/ChatPage';
+import SheetPage from './components/SheetPage';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/chat" element={<ChatPage />} />
+        <Route path="/sheet" element={<SheetPage />} />
       </Routes>
     </Router>
   );
